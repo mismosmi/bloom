270b0167d16d6e7faa69ccf6d66b367076ca3b2a
Michel Smola <michel.smola@farbenmeer.de>
2024-08-23 12:34:32 +0200

    feat: use component macro for client-example

--- bloom-client-example/src/lib.rs
+++ bloom-client-example/src/lib.rs
@@ -1,8 +1,7 @@
 use std::{panic, sync::Arc};
 
-use async_trait::async_trait;
 use bloom_client::{get_element_by_id, render};
-use bloom_core::{use_effect, use_ref, use_state, Component, Element};
+use bloom_core::{use_effect, use_ref, use_state};
 use bloom_html::{tag, tag::div, DomRef, HtmlNode};
 use bloom_rsx::rsx;
 use wasm_bindgen::prelude::*;
@@ -22,34 +21,34 @@ fn main() -> Result<(), JsValue> {
     Ok(())
 }
 
-#[derive(PartialEq)]
-struct ExampleApp;
+#[bloom_core::component]
+fn ExampleApp() -> Result<Element<HtmlNode, ()>, ()> {
+    console::log_1(&"Rendering ExampleApp".into());
+    console::log_1(&format!("{:?}", div().build()).into());
+    let hello_world_ref: Arc<DomRef> = use_ref();
 
-#[async_trait]
-impl Component for ExampleApp {
-    type Node = HtmlNode;
-    type Error = ();
+    use_effect(hello_world_ref.clone(), |node| {
+        console::log_2(&"hello world div".into(), &node.get().unwrap());
+    });
 
-    async fn render(self: Arc<Self>) -> Result<Element<Self::Node, Self::Error>, Self::Error> {
-        console::log_1(&"Rendering ExampleApp".into());
-        console::log_1(&format!("{:?}", div().build()).into());
-        let hello_world_ref: Arc<DomRef> = use_ref();
-
-        use_effect(hello_world_ref.clone(), |node| {
-            console::log_2(&"hello world div".into(), &node.get().unwrap());
-        });
-
-        let counter = use_state(|| 0i32);
-        Ok(rsx!(
-            <div>
-                <div ref=hello_world_ref>
-                    "Hello, World!"
-                </div>
-                <div>{counter.to_string()}</div>
-                <button on_click=move |_| counter.update(|count| *count + 1)>
-                    "Increase"
-                </button>
+    let counter = use_state(|| 0i32);
+    Ok(rsx!(
+        <div>
+            <div ref=hello_world_ref>
+                "Hello, World!"
             </div>
-        ))
-    }
+            <div>{counter.to_string()}</div>
+            <button on_click=move |_| counter.update(|count| *count + 1)>
+                "Increase"
+            </button>
+            <MacroComponent label="Hello, Macro!" />
+        </div>
+    ))
+}
+
+#[bloom_core::component]
+fn MacroComponent(label: String) -> Result<Element<HtmlNode, ()>, ()> {
+    Ok(rsx!(
+        <div>{label}</div>
+    ))
 }
